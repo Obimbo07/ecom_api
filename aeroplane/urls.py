4c604115fd4b49f43bdb1aaf29f839f285503ad4
Austin Obimbo <obimboausts@gmail.com>
2025-03-13 14:19:49 +0300

    created models urls

--- aeroplane/urls.py
+++ aeroplane/urls.py
@@ -1,15 +1,26 @@
 from django.contrib import admin
-from django.urls import include, path
-from django.conf import settings
-from django.contrib.staticfiles import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from aeroplane.views import (
+    ProductViewSet, CategoryViewSet, CartViewSet, create_checkout, 
+    create_order, get_user_orders, create_checkout_session_view, 
+    mpesa_callback_view, query_mpesa_view
+)
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='product')
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'cart', CartViewSet, basename='casrt')
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("ckeditor5/", include('django_ckeditor_5.urls')),
-
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('users/', include('users.urls')),
+    path('api/checkout/', create_checkout),
+    path('api/orders/', create_order),
+    path('api/user/orders/', get_user_orders),
+    path('api/checkout-session/', create_checkout_session_view),
+    path('mpesa-callback/', mpesa_callback_view),
+    path('api/query-mpesa/', query_mpesa_view),
+    path('ckeditor5/', include('django_ckeditor_5.urls')),
 ]
-
-if settings.DEBUG:
-    from django.urls import re_path
-
-    urlpatterns += [re_path(r"^static/(?P<path>.*)$", views.serve)]
